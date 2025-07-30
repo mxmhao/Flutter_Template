fa1b296ed915b3dd0a8dcd3a183d78025ee0adc2
maoxm <mxmhao@163.com>
2025-07-30 20:00:38 +0800

    添加模版

--- lib/template/templates.dart
+++ lib/template/templates.dart
@@ -1,5 +1,10 @@
+import 'dart:typed_data';
+
+import 'package:dart_pdf_reader/dart_pdf_reader.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer';
+import 'dart:developer' show log;
+
+import 'package:image/image.dart' as img;
 
 // PopScope 不放顶级，放子组件也行
 class PopScopeDemo extends StatelessWidget {
@@ -13,7 +18,7 @@ class PopScopeDemo extends StatelessWidget {
       ),
       body: Column(
         children: [
-          // PopScope 不是非得套在顶层才有阻止 pop 效果，放在小部件中也行
+          // PopScope 不是非得套在顶层才有阻止 pop 效果，放在小部件中也行，child 最好用Container()，其他的可能导致白屏
           PopScope(canPop: false, child: Container(), onPopInvokedWithResult: (bool didPop, _) {
             log("onPopInvokedWithResult");
           }),
@@ -88,5 +93,61 @@ class FractionallySizedBoxDemo extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+// 使用的 dart_pdf_reader 库
+// PDFPageObjectNode page = pages.getPageAtIndex(0);
+// _parseBarcode(page);
+// 此提取的是pdf中的资源，有些图像非资源，无法用此方法提取
+// 解析 页面中的 barcode
+Future<void> parseBarcode(PDFPageObjectNode page) async {
+  // 获取资源
+  final resources = await page.resources;
+  final xObject = resources?[PDFNames.xObject];
+  if (null == xObject || xObject is! PDFDictionary) {
+    return;
+  }
+
+  Uint8List? imageData;
+  PDFObject? streamObject;
+  PDFObject? filter;
+  PDFNumber width;
+  PDFNumber height;
+  final entries = xObject.entries.entries;
+  for (var entry in entries) {
+    streamObject = entry.value;
+    if (streamObject is! PDFObjectReference) {
+      continue;
+    }
+
+    streamObject = await page.objectResolver.resolve(streamObject);
+    if (streamObject is! PDFStreamObject || streamObject.dictionary[PDFNames.subtype] != PDFNames.image) {
+      continue;
+    }
+    // final imageRaw = await streamObject.readRaw();
+    // Log.d('resource: ${imageRaw.length}');
+    // final imageData = await read(streamObject, page.objectResolver);
+
+    try {
+      imageData = await streamObject.read(page.objectResolver);
+    } catch (e) {
+      // Log.e("无法解码的图片: $e");
+      continue;
+    }
+    filter = streamObject.dictionary[PDFNames.filter];
+    // dctDecode 是未解码的jpg图片
+    if (filter == PDFNames.dctDecode || (filter is PDFArray && filter.contains(PDFNames.dctDecode))) {
+      // Log.d('resource: ${imageData.length}');
+      // JPG 解码
+      final image = img.decodeImage(imageData);
+      imageData = image?.data?.buffer.asUint8List();
+    }
+    if (null == imageData || imageData.isEmpty) return;
+
+    // 宽高
+    width = streamObject.dictionary[PDFNames.width] as PDFNumber;
+    height = streamObject.dictionary[PDFNames.height] as PDFNumber;
+    // Log.d('imageData: ${imageData.length}, ${width.toInt()}, ${height.toInt()}');
   }
 }
